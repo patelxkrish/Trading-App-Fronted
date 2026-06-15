05bb19bb070d6696bc08db71e89fabbd81222430
krish <www.kanubhai118@gmail.com>
2026-06-15 18:03:00 +0530

    fix hero image path

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div className="container p-5 mb-5 mt-5">
       <div className="row text-center">
-        <img src="/assets/homehero.png" className="mb-5" alt="heroImage" />
+        <img src="/assets/homeHero.png" className="mb-5" alt="heroImage" />
         <h1 className="mt-5">Invest in everything</h1>
         <p>
           Online platform to invest in stocks,derivatives,mutual funds,and more
